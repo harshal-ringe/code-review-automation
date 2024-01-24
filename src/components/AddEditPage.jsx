0566b581cd3f28d3e955cd200da862481a06199a
Harshal Ringe <ringeharshal@gmail.com>
2024-01-24 11:04:10 +0530

    Unused imprts, variables, not formatted, console logs

--- src/components/AddEditPage.jsx
+++ src/components/AddEditPage.jsx
@@ -1,15 +1,17 @@
-import { Loadable } from 'react-loadable';
 import { Link, HashRouter } from 'react-router-dom';
+import { Loadable } from 'react-loadable';
 import React, { Component } from 'react';
 
 class AddEditPage extends Component {
 	constructor(props) {
 		super(props);
 		let y = '123';
+		this.variable = 'variable'
 	}
 	componentDidMount(){
 		let x= 'abc'
 			const c = 123
+	var v = null;
 
 		$.ajax({
 			async: false,
@@ -17,19 +19,18 @@ class AddEditPage extends Component {
 			success: function (response) {
 				console.log(response);
 			},
+			error: function() {
+
+			}
 		});
 	}
-	componentWillUnmount(){
-
-	}
-	componentWillReceiveProps() {
-
-	}
+	componentWillUnmount(){}
+	componentWillReceiveProps() {}
 	render(){
 		return (
 			<>
 
-		<div>
+	<div>
 		<div><div><div style={{fontSize: 15, letterSpacing: 2, marginLeft: 15, marginRight: 15}}>Hello World</div></div></div></div>
 			</>
 		);
