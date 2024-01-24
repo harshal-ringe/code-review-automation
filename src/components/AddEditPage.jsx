9d55469efbd82e198d1b4e93c151e4961441ba53
Harshal Ringe <ringeharshal@gmail.com>
2024-01-24 11:11:42 +0530

    Unused imoprts, variables, not formatted, console logs

--- src/components/AddEditPage.jsx
+++ src/components/AddEditPage.jsx
@@ -1,17 +1,17 @@
-import { Link, HashRouter } from 'react-router-dom';
 import { Loadable } from 'react-loadable';
+import { Link, HashRouter } from 'react-router-dom';
 import React, { Component } from 'react';
 
 class AddEditPage extends Component {
 	constructor(props) {
 		super(props);
+		this.variable = 'variable';
 		let y = '123';
-		this.variable = 'variable'
 	}
-	componentDidMount(){
-		let x= 'abc'
-			const c = 123
-	var v = null;
+	componentDidMount() {
+		var v = null;
+		let x = 'abc';
+		const c = 123;
 
 		$.ajax({
 			async: false,
@@ -19,22 +19,31 @@ class AddEditPage extends Component {
 			success: function (response) {
 				console.log(response);
 			},
-			error: function() {
-
-			}
+			error: function () {},
 		});
 	}
-	componentWillUnmount(){}
-	componentWillReceiveProps() {}
-	render(){
+	
+	componentWillUnmount() {
+
+	}
+
+	componentWillReceiveProps() {
+		
+	}
+
+	render() {
 		return (
 			<>
-
-	<div>
-		<div><div><div style={{fontSize: 15, letterSpacing: 2, marginLeft: 15, marginRight: 15}}>Hello World</div></div></div></div>
+				<div>
+					<div>
+						<div>
+<div style={{ fontSize: 15, letterSpacing: 2, marginLeft: 15, marginRight: 15 }}>Hello World</div>
+						</div>
+					</div>
+				</div>
 			</>
 		);
 	}
 }
 
-export default AddEditPage
+export default AddEditPage;
